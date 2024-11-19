bc65eb21854610fd188dbc92300a2c747c5a6a75
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-11-19 11:53:27 +0100

    Adding Image Features

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,21 @@
-import {Text, View} from 'react-native';
+import {Text, View, Image, ImageBackground} from 'react-native';
+
+const logoImage = require("./images/favicon.png")
 
 export default function App() {
   return <View style={{flex: 1,backgroundColor:'plum', padding: 60}}>
    <Text>
     <Text style={{color: 'white'}}>Hello</Text> World!
     </Text>
+    {/*
+    <Image source={logoImage} style={{width: 300, height: 300}}></Image>
+    <Image 
+    source={{uri: 'https://picsum.photos/200/300'}} 
+    style={{width: 300, height: 300}}
+
+    /> */}
+    <ImageBackground source={logoImage}>
+      <Text>IMAGE TEXT</Text>
+    </ImageBackground>
   </View>
 }
