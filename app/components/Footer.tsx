7afe0aeb9e3f7d28f702c967616607c7a240c55a
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-11-27 14:59:39 +0100

    Third update

---
+++ app/components/Footer.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default function Footer() {
+    return (
+        <View style={{backgroundColor: "yellow",}}>
+            <Text style={{textAlign:"center", margin:"10px"}}>All rights reserved by Little Lemon</Text>
+        </View>
+    )
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
+import LittleLemonHeader from './components/LittleLemonHeader';
+import Footer from './components/Footer';
 
-export default function LittleLemonHeader(){
-  return(
-    <View style={{flex: 0.2, backgroundColor: '#F4CE14'}} numberOfLines={1}>
-      <Text>Welcome to <Text style={{fontWeight: 'bold'}}>Little Lemon</Text>
-      </Text>
+export default function Index(){
+  return (
+    <View
+    style={{flex:1, backgroundColor: '#495E57'}}>
+         <LittleLemonHeader />
+         <Text style={{color:'white',textAlign:'center',margin:'10px'}}>Little Lemon is a charming neighborhood 
+          bistro that serves simple food and classic cocktailss in a 
+          lively but casual environment. We would love to hear
+          more about your experience with us!
+         </Text>
+         <Footer />
     </View>
-  );
+
+  )
 }
