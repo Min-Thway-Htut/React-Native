dec915097eb6c6459f24eaf5db1faadcaa1d0cae
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-11-27 14:06:55 +0100

    Second update

--- app/index.tsx
+++ app/index.tsx
@@ -1,51 +1,11 @@
-import {StyleSheet, Text, View, ScrollView, FlatList} from "react-native";
-import React, {useState} from 'react';
+import * as React from 'react';
+import { View, Text } from 'react-native';
 
-export default function Index() { 
-
-  const [people, setPeople] = useState([
-    {name: 'Person1', key: '1'},
-    {name: 'Person2', key: '2'},
-    {name: 'Person3', key: '3'},
-    {name: 'Person4', key: '4'},
-    {name: 'Person5', key: '5'},
-    {name: 'Person6', key: '6'},
-    {name: 'Person7', key: '7'},
-  ])
-
-  return (
-    <View style={styles.container}>
-
-      <FlatList
-      numColumns={2}
-      data={people} renderItem={({item}) => (
-        <Text style={styles.item}>{item.name}</Text>
-      )}/>
-     {/* <ScrollView>
-       {people.map((item) => {
-        return (
-          <View key={item.key}>
-            <Text style={styles.item}>{item.name}</Text>
-          </View>
-        )
-      })}
-      </ScrollView>  */}
+export default function LittleLemonHeader(){
+  return(
+    <View style={{flex: 0.2, backgroundColor: '#F4CE14'}} numberOfLines={1}>
+      <Text>Welcome to <Text style={{fontWeight: 'bold'}}>Little Lemon</Text>
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  item:{
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-  }
-});
