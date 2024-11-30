acc66d37db11fb7176234006aa77d3ea42064ffa
Min-Thway-Htut <minthwayhtut568@gmail.com>
2024-11-30 17:53:54 +0100

    Another update

---
+++ app/components/MenuItems.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import {View, Text, ScrollView} from "react-native";
+
+const green = "#495E57";
+const yellow =  "#F4CE14";
+
+const menuItemsToDisplay = [
+    'Apple \n Orange \n Banana \n Lemon \n'
+]
